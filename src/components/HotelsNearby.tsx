9d089f087d4bb19603bce5dbbd0cda992878605f
matt2mi <2m1tema@gmail.com>
2018-01-28 22:51:58 +0100

    clean package.json dep - carousel pour liste des gîtes (+photos)

--- src/components/HotelsNearby.tsx
+++ src/components/HotelsNearby.tsx
@@ -1,23 +1,154 @@
 import * as React from 'react';
-import CardText from 'reactstrap/lib/CardText';
-import CardTitle from 'reactstrap/lib/CardTitle';
-import Card from 'reactstrap/lib/Card';
-import Col from 'reactstrap/lib/Col';
-import Row from 'reactstrap/lib/Row';
+import {
+    Carousel,
+    CarouselCaption,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselItem,
+    Col,
+    Jumbotron,
+    Row
+} from 'reactstrap';
 
-export default class HotelsNearby extends React.Component {
+interface Props {
+}
+
+interface State {
+    readonly activeIndex: number;
+}
+
+const photo1 = require('../img/hostel1-big.jpg');
+const photo2 = require('../img/hostel2-big.jpg');
+const photo3 = require('../img/hostel3-big.jpg');
+const items = [
+    {
+        src: photo1,
+        captionText: '53800 St Saturnin du Limet',
+        captionHeader: 'Appartements Le Hardas',
+        href: 'https://www.gites.fr/gites_le-hardas-mercure_saint-saturnin-du-limet_h668613.htm'
+    },
+    {
+        src: photo2,
+        captionText: '53800 Renazé',
+        captionHeader: 'Maison de vacances La Petite Coquais',
+        href: 'https://www.gites.fr/gites_holiday-home-la-petite-coquais-05_renaze_h821078.htm'
+    },
+    {
+        src: photo3,
+        captionText: '53390 La Rouaudière',
+        captionHeader: 'Maison de vacances La Rouaudiere',
+        href: 'https://www.gites.fr/gites_holiday-home-la-rouaudiere-with-fireplace-i_la-rouaudiere_h1093959.htm'
+    }
+];
+
+export default class HotelsNearby extends React.Component<Props, State> {
+
+    animating: boolean;
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            activeIndex: 0
+        };
+
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) {
+            return;
+        }
+        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({activeIndex: nextIndex});
+    }
+
+    previous() {
+        if (this.animating) {
+            return;
+        }
+        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        this.setState({activeIndex: nextIndex});
+    }
+
+    goToIndex(newIndex: number) {
+        if (this.animating) {
+            return;
+        }
+        this.setState({activeIndex: newIndex});
+    }
+
     render() {
+        const {activeIndex} = this.state;
+
+        const slides = items.map((item) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={item.src}
+                >
+                    <img src={item.src} className="img-fluid img-500" alt={item.captionHeader}/>
+                    <CarouselCaption captionText={item.captionText} captionHeader={item.captionHeader}/>
+                </CarouselItem>
+            );
+        });
+
         return (
             <div>
                 <Row>
                     <Col sm="12">
-                        <Card body={true}>
-                            <CardTitle>Gîtes</CardTitle>
-                            <CardText>Les gîtes à proximité du lieu.</CardText>
-                        </Card>
+                        <Jumbotron>
+                            <Carousel
+                                activeIndex={activeIndex}
+                                next={this.next}
+                                previous={this.previous}
+                            >
+                                <CarouselIndicators
+                                    items={items}
+                                    activeIndex={activeIndex}
+                                    onClickHandler={this.goToIndex}
+                                />
+                                {slides}
+                                <CarouselControl
+                                    direction="prev"
+                                    directionText="Previous"
+                                    onClickHandler={this.previous}
+                                />
+                                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next}/>
+                            </Carousel>
+                        </Jumbotron>
                     </Col>
                 </Row>
             </div>
         );
     }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <Row>
+    //                 <Col sm="12">
+    //                     <Card body={true}>
+    //                         <CardTitle>Gîtes</CardTitle>
+    //                         <CardText>Les gîtes à proximité du lieu.</CardText>
+    //                         <img src={photo}/>
+    //                     </Card>
+    //                 </Col>
+    //             </Row>
+    //         </div>
+    //     );
+    // }
 }
